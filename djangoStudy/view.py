26c4d2e612d265983c02fbe1c59ebe877f490e6e
Jay <quantong_xu@163.com>
2020-08-25 21:47:11 +0800

    add test

--- djangoStudy/view.py
+++ djangoStudy/view.py
@@ -4,3 +4,6 @@ from django.template.context_processors import request
 
 def hello(request):
     return HttpResponse("Hello World!");
+
+def hello2(request):
+    return HttpResponse("Hello world2 ! ")
